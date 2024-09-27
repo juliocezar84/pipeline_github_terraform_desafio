fc53e4cd0684426ee19545155f9ed9a830e7f387
Julio Vicente <juliocezar84@gmail.com>
2024-09-27 02:51:32 -0300

    alteração

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,6 @@
-module "bucket"{
-    source = "./modules/bucket-aws"
-    bucket_prefix = "terraform-bucket-posmack"
+module "ec2"{
+    source = "./modules/ec2-aws"
+    ec2_prefix = "terraform-ec2-posmack"
     region = "us-east-1"
 
 }
